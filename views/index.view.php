ab6012a4b0ccd710535a5db326ff9e89e3b7b5f0
sahAlves <sah.alves002@gmail.com>
2025-08-09 01:10:21 -0300

    iniciando tela principal

---
+++ views/index.view.php
@@ -0,0 +1,41 @@
+<div class="flex justify-between items-center mb-20">
+    <div class="bg-[url(./images/Logo.png)] bg-contain bg-no-repeat w-[48px] h-[48px]"></div>
+    <div class="flex">
+        <ul class="flex space-x-4 font-bold">
+            <li>
+                <a href="/" class="text-pink-500 flex gap-2 bg-gray-700 rounded-lg p-2">
+                    <img src="./images/popcorn.svg" alt="">
+                    Explorar
+                </a>
+            </li>
+            <li>
+                <a href="/meus-filmes" class="flex gap-2 p-2 hover:bg-gray-900 rounded-lg">
+                    <img src="./images/movie.svg" alt="">
+                    Meus Filmes
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="flex gap-2 items-center">
+        Olá, Jordan
+        <div class="bg-[url(./images/avatar.svg)] bg-no-repeat w-[32px] h-[32px]"></div>
+        <div class="border-l h-6 mx-2 border-gray-500"></div>
+        <div class="bg-gray-800 rounded-lg p-[6px]">
+            <div class="bg-[url(./images/logout.svg)] bg-no-repeat w-[15px] h-[15px] "></div>
+        </div>
+    </div>
+</div>
+
+<div>
+    <div class="flex justify-between mb-10">
+        <h1>Explorar</h1>
+        <div>Pesquisar filme</div>
+    </div>
+    <div class="grid grid-cols-4 gap-4">
+        <div>filme</div>
+        <div>filme</div>
+        <div>filme</div>
+        <div>filme</div>
+        <div>filme</div>
+    </div>
+</div>
